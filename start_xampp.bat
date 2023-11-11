e0fe92c5c35a6bb9a9f10a821ae7c4ff61cd3317
prisma7771 <prismaputra91@gmail.com>
2023-11-12 01:14:14 +0700

    Signed-off-by: prisma7771 <prismaputra91@gmail.com>

--- start_xampp.bat
+++ start_xampp.bat
@@ -1,14 +1,20 @@
 @echo off
-cd /d E:\Programming\xampp
+
+:: Read configuration from file
+set "CONFIG_FILE=config.txt"
+for /f "tokens=1,* delims==" %%A in (%CONFIG_FILE%) do set "%%A=%%B"
+
+:: Change to XAMPP Folder
+cd /d %XAMPP_PATH%
 
 :: Start MySQL server
 start /min xampp-control.exe
 
-:: Open a new command prompt window to run npm run start
-start /min cmd /k "cd /d E:\Programming\Web\Vue\form_input\backend && php artisan serve --host 0.0.0.0"
+:: Open a new command prompt window to run Laravel backend
+start /min cmd /k "cd /d %BACKEND_PATH% && php artisan serve --host 0.0.0.0"
 
-:: Open a new command prompt window to run npm run start
-start /min cmd /k "cd /d E:\Programming\Web\Vue\form_input\frontend && npm run dev -- --host 0.0.0.0"
+:: Open a new command prompt window to run Vue.js frontend
+:: start /min cmd /k "cd /d %FRONTEND_PATH% && npm run dev -- --host 0.0.0.0"
 
 :: Add a delay to give the Vue.js app time to start (adjust the delay as needed)
 timeout /t 10
